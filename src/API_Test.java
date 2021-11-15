286db744d173aa5491169980f1ce51dabbcf2829
Ahmad Malloh <ahmadmalloh97@gmail.com>
2021-11-15 22:10:50 +0200

    test

---
+++ src/API_Test.java
@@ -0,0 +1,21 @@
+import io.restassured.RestAssured;
+
+public class API_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		RestAssured.baseURI="";
+		
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		
+		
+		
+
+	}
+
+}
