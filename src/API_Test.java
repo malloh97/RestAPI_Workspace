f00a8d27ac7dae9be3e04fc319f9e0fc55101884
Ahmad Malloh <ahmadmalloh97@gmail.com>
2021-11-20 21:30:59 +0200

    new

--- src/API_Test.java
+++ src/API_Test.java
@@ -1,16 +1,59 @@
 import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 
 public class API_Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 		
-		RestAssured.baseURI="";
+		RestAssured.baseURI="http://localhost:3000";
 		
+		/*
+		 * Post New Customer
+		 * 
+		 */
 		
+		given().headers("Content-Type","application/json").body("{\r\n" + 
+				"    \"name\": \"Ali\",\r\n" + 
+				"    \"age\": \"26\",\r\n" + 
+				"    \"Job\": \"Automation Test Engineer\",\r\n" + 
+				"    \"Experience\": \"Three year\",\r\n" + 
+				"    \"Location\": \"Jordan\"\r\n" + 
+				"  }").when().post("/Customer")
 		
+		.then().log().all().assertThat().statusCode(201);
+
+		/*
+		 * Update Exist Customer
+		 * 
+		 */
 		
 
+		given().param("name", "Ali").headers("Content-Type","application/json").body("{\r\n" + 
+				"    \"name\": \"Ali\",\r\n" + 
+				"    \"age\": \"34\",\r\n" + 
+				"    \"Job\": \"Automation Test Engineer\",\r\n" + 
+				"    \"Experience\": \"six years\",\r\n" + 
+				"    \"Location\": \"Jordan\"\r\n" + 
+				"  }")
+		
+		.when().put("/Customer")
+		
+		.then().log().all().assertThat().statusCode(200);
+		
+		
+		/*
+		 * Get the Customer
+		 * 
+		 */
+		
+		given().param("name", "Ali").headers("Content-Type","application/json")
+		
+		.when().get("/Customer")
+		
+		.then().log().all().assertThat().statusCode(200); 
+		
+		
 	}
 
 }
